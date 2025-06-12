05279555a6d932d136a03810f943d3841464f39f
Sally Keating <salkeating@deloitte.ca>
2025-06-12 11:11:29 -0400

    updated files

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/salkeating/Library/CloudStorage/OneDrive-Deloitte(O365D)/Desktop/utilitx/utilitx-ui/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/salkeating/Library/CloudStorage/OneDrive-Deloitte(O365D)/Desktop/utilitx/utilitx-ui/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
